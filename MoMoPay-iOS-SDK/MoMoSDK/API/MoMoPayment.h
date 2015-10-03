c8371727bbcda74174625210c44ff586d1d9eb6e
lanhmomo <lanh.luu@mservice.com.vn>
2015-10-03 14:57:37 +0700

    Add Document

---
+++ MoMoPay-iOS-SDK/MoMoSDK/API/MoMoPayment.h
@@ -0,0 +1,19 @@
+//
+//  MoMoPayment.h
+//  SampleApp-Xcode
+//
+//  Created by Luu Lanh on 9/30/15.
+//  Copyright (c) 2015 LuuLanh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface MoMoPayment : NSObject
++(MoMoPayment*)shareInstant;
+-(void)requestPayment:(NSMutableDictionary*)parram;
+-(void)requestToken;
+-(void)handleOpenUrl:(NSURL*)url;
+-(void)createPaymentInformation:(NSMutableDictionary*)info;
+-(void)addMoMoPayDefaultButtonToView:(UIView*)parrentView;
+-(UIButton*)addMoMoPayCustomButton:(UIButton*)button forControlEvents:(UIControlEvents)controlEvents toView:(UIView*)parrentView;
+@end
