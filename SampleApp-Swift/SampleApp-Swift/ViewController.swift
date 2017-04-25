a855eedde072176b8cc8893dae7eecc8de1f3c9c
Github.MoMoSDK <lanh.luu@mservice.com.vn>
2017-04-26 03:32:32 +0700

    Fix layout

--- SampleApp-Swift/SampleApp-Swift/ViewController.swift
+++ SampleApp-Swift/SampleApp-Swift/ViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
+    
     var lblMessage: UILabel!
     var txtAmount: UITextField!
-    var btnPay :UIButton!
+    var btnPay: UIButton!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         NotificationCenter.default.addObserver(self, selector: #selector(self.NoficationCenterTokenReceived), name:NSNotification.Name(rawValue: "NoficationCenterTokenReceived"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.NoficationCenterCreateOrderReceived), name:NSNotification.Name(rawValue: "NoficationCenterCreateOrderReceived"), object: nil)
         
-        initLayout()
+        continueToPaymentOrder()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     /*
      * SERVER SIDE DEMO
@@ -39,12 +39,14 @@ class ViewController: UIViewController {
         print("::MoMoPay Log::Received Token Replied::\(notif.object!)")
         lblMessage.text = "RequestToken response:\n  \(notif.object as Any)"
         
-        let response:NSDictionary = notif.object! as! NSDictionary
+        let response:NSMutableDictionary = notif.object! as! NSMutableDictionary
         
         
-        let _status = response["status"] as? Int
         
-        if _status == 0 {
+        //let _status = response["status"] as! String
+        let _statusStr = "\(response["status"] as! String)"
+        
+        if (_statusStr == "0") {
             
             print("::MoMoPay Log: SUCESS TOKEN. CONTINUE TO CALL API PAYMENT")
             print(">>phone \(response["phonenumber"] as! String)   :: data \(response["data"] as! String)")
@@ -59,96 +61,16 @@ class ViewController: UIViewController {
             orderInfo.setValue(merchant_username,			forKey: "username");
             orderInfo.setValue(response["phonenumber"] as! String,			forKey: "phonenumber");
             orderInfo.setValue(response["data"] as! String,			forKey: "data");
-
-            lblMessage.text = "Processing..."
+            
+            lblMessage.text = "Get token success! Processing payment..."
             submitOrderToServer(parram: orderInfo)
             
-            lblMessage.text = "RequestToken response:\n \(notif.object!) | Please continue to proccess on server "
         }
         else{
             lblMessage.text = "RequestToken response:\n \(notif.object!) | Fail token. Please check input params "
         }
     }
     
-    func submitOrderToServer(parram: NSMutableDictionary) {
-        btnPay.backgroundColor = UIColor.gray
-        btnPay.isEnabled = false
-        /*Send client request to your Server here*/
- 
-        /*
-        let clientPakageDefine = "{\"data\":\"\(parram["data"] as! String)\",\"merchantcode\":\"\(MoMoConfig.getMerchantcode())\",\"amount\":\"\(parram["amount"] as! String)\",\"username\":\"\(parram["username"] as! String)\",\"email\":\"\(parram["email"] as! String)\"}"
-        
-        do {
-            
-            let jsonData = try JSONSerialization.data(withJSONObject: clientPakageDefine, options: .prettyPrinted)
-            
-            // create post request
-            let url = NSURL(string: "http://www.your_domain_api.com/api")!
-            let request = NSMutableURLRequest(url: url as URL)
-            request.httpMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.setValue("gzip", forHTTPHeaderField: "Accept-Encoding")
-            request.httpBody = jsonData
-            
-            let task = URLSession.shared.dataTask(with: request as URLRequest){ data, response, error in
-                if error != nil{
-                    //print("Error -> \(String(describing: error))")
-         
-                }
-                
-                do {
-                    //print("Success -> \(response)")
-                    
-                } catch {
-                    //print("Error -> \(error)")
-                }
-            }
-            
-            task.resume()
-            
-        } catch {
-            print(error)
-        } */
-        
-        
-/**********BEGIN Sample send request on Your Server -To - MoMo Server
- **********WARNING: need to remove it on your product app
- **********/
-        let merchant_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkpa+qMXS6O11x7jBGo9W3yxeHEsAdyDE40UoXhoQf9K6attSIclTZMEGfq6gmJm2BogVJtPkjvri5/j9mBntA8qKMzzanSQaBEbr8FyByHnf226dsLt1RbJSMLjCd3UC1n0Yq8KKvfHhvmvVbGcWfpgfo7iQTVmL0r1eQxzgnSq31EL1yYNMuaZjpHmQuT24Hmxl9W9enRtJyVTUhwKhtjOSOsR03sMnsckpFT9pn1/V9BE2Kf3rFGqc6JukXkqK6ZW9mtmGLSq3K+JRRq2w8PVmcbcvTr/adW4EL2yc1qk9Ec4HtiDhtSYd6/ov8xLVkKAQjLVt7Ex3/agRPfPrNwIDAQAB"
-        let merchant_code       = "SCB01"
-        let merchant_transaction_id = "123456789"
-        let merchant_bill_id        = "bill1001"
-        let merchant_username       = "username_or_email_or_fullname"
-        let merchant_server_ip       = "192.16.17.120"
-        var your_hash_value_sample = "RSA_orderInfo_by_public_key" ;// RSA orderInfo json using MoMo's Public key
-        
-        // tag name to access the stored public key in keychain
-        let TAG_PUBLIC_KEY = ""
-        
-        let hash_plaintext = "{\"merchantcode\":\"\(merchant_code)\",\"transid\":\"\(merchant_transaction_id)\",\"billid\":\"\(merchant_bill_id)\",\"amount\":10000,\"fee\":0,\"phonenumber\":\"\(parram["phonenumber"] as! String)\",\"username\":\"\(merchant_username)\"}"
-        
-        let hash_encrypted = RSAUtils.encryptWithRSAPublicKey(hash_plaintext.data(using: String.Encoding.utf8)!, pubkeyBase64: merchant_public_key, keychainTag: TAG_PUBLIC_KEY)
-        if ( hash_encrypted == nil ) {
-            print("Error while encrypting ")
-        } else {
-            your_hash_value_sample = hash_encrypted!.base64EncodedString(options: NSData.Base64EncodingOptions())
-            print("::Encrypted =  \(your_hash_value_sample)")
-        }
-        
-        
-        let dataPayment : NSMutableDictionary = NSMutableDictionary()
-        dataPayment .setValue(merchant_code,                       forKey: "merchantcode")
-        dataPayment.setValue(your_hash_value_sample,		 forKey: "hash");
-        dataPayment.setValue(merchant_server_ip,		 forKey: "ipaddress");
-        dataPayment.setValue(parram["data"] as! String,		 forKey: "data");
-        dataPayment.setValue(parram["phonenumber"] as! String,	 forKey: "phonenumber");
-        
-        MoMoPayment.sharedInstance.requestPayment(parram: parram)
-/**********END Sample send request on Your Server -To - MoMo Server
-**********WARNING: need to remove it on your product app
-**********/
-        
-    }
     
     func NoficationCenterCreateOrderReceived(notif: NSNotification) {
         
@@ -163,12 +85,12 @@ class ViewController: UIViewController {
             return;
         }
         print("::MoMoPay Log::Request Payment Replied::\(notif.object as Any)")
-
+        
         
         if (notif.object! is NSDictionary) {
             
             let response:NSDictionary = notif.object! as! NSDictionary
-
+            
             let _status = response["status"] as? Int
             
             if _status == 0 {
@@ -178,17 +100,17 @@ class ViewController: UIViewController {
                 print("::MoMoPay Log::Payment Error::\(response["message"] as! String)")
             }
             
-            lblMessage.text = "RequestPayViaMoMo response:\n\n status: \(String(describing: _status)) \n message: \(response["message"] as! String)"
+            lblMessage.text = "Result:\n\n status: \(String(describing: _status)) \n message: \(response["message"] as! String)"
             
             //continue your checkout order here
         }
         else{
-            lblMessage.text = "RequestPayViaMoMo response:\n \(notif.object as Any)"
+            lblMessage.text = "Result:\n \(notif.object as Any)"
         }
     }
     
     func dictionaryFromUrlParram(_ urlPararm: String) -> NSDictionary {
-       
+        
         let mapdata = NSMutableDictionary();
         
         //var params = NSMutableDictionary()
@@ -211,7 +133,7 @@ class ViewController: UIViewController {
     }
     
     
-    func initLayout() {
+    func continueToPaymentOrder() {
         
         let codedLabel:UILabel = UILabel()
         codedLabel.frame = CGRect(x: 10, y: 50, width: 300, height: 30)
@@ -265,7 +187,6 @@ class ViewController: UIViewController {
         paymentArea.addSubview(line)
         
         //Tạo button Thanh toán bằng Ví MoMo
-        //var btnPay:UIButton = UIButton()
         btnPay = UIButton()
         btnPay.frame = CGRect(x: 10, y: 100, width: 260, height: 40)
         btnPay.setTitle("Pay Via MoMo Wallet", for: .normal)
@@ -276,7 +197,7 @@ class ViewController: UIViewController {
         
         //let lblMessage:UILabel = UILabel()
         lblMessage = UILabel()
-        lblMessage.frame = CGRect(x: 10, y: 120, width: 300, height: 200)
+        lblMessage.frame = CGRect(x: 60, y: 100, width: 300, height: 200)
         lblMessage.text = "{MoMo Response}"
         lblMessage.font = UIFont.systemFont(ofSize: 15)
         lblMessage.backgroundColor = UIColor.clear
@@ -294,7 +215,7 @@ class ViewController: UIViewController {
         
         //Buoc 1: Khoi tao Payment info, add button MoMoPay
         let paymentinfo = NSMutableDictionary()
-        paymentinfo[MOMO_PAY_CLIENT_MERCHANT_TRANS_ID] = ""
+        paymentinfo[MOMO_PAY_CLIENT_MERCHANT_TRANS_ID] = "41129898989"
         paymentinfo[MOMO_PAY_CLIENT_AMOUNT_TRANSFER] = amount
         paymentinfo[MOMO_PAY_CLIENT_FEE_TRANSFER] = fee
         paymentinfo[MOMO_PAY_CLIENT_USERNAME] = username
@@ -304,8 +225,91 @@ class ViewController: UIViewController {
         //Buoc 2: add button Thanh toan bang Vi MoMo vao khu vuc ban can hien thi (Vi du o day la vung paymentArea)
         ///Default MoMo Button [[MoMoPayment shareInstant] addMoMoPayDefaultButtonToView:paymentArea];
         ///Custom button
-
+        
+        
+        
+        //Code của bạn
         self.view.addSubview(paymentArea)
+    }
+    
+    
+    func submitOrderToServer(parram: NSMutableDictionary) {
+        btnPay.backgroundColor = UIColor.gray
+        btnPay.isEnabled = false
+        /*Send client request to your Server here*/
+        
+        /*
+         let clientPakageDefine = "{\"data\":\"\(parram["data"] as! String)\",\"merchantcode\":\"\(MoMoConfig.getMerchantcode())\",\"amount\":\"\(parram["amount"] as! String)\",\"username\":\"\(parram["username"] as! String)\",\"email\":\"\(parram["email"] as! String)\"}"
+         
+         do {
+         
+         let jsonData = try JSONSerialization.data(withJSONObject: clientPakageDefine, options: .prettyPrinted)
+         
+         // create post request
+         let url = NSURL(string: "http://www.your_domain_api.com/api")!
+         let request = NSMutableURLRequest(url: url as URL)
+         request.httpMethod = "POST"
+         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+         request.setValue("gzip", forHTTPHeaderField: "Accept-Encoding")
+         request.httpBody = jsonData
+         
+         let task = URLSession.shared.dataTask(with: request as URLRequest){ data, response, error in
+         if error != nil{
+         //print("Error -> \(String(describing: error))")
+         
+         }
+         
+         do {
+         //print("Success -> \(response)")
+         
+         } catch {
+         //print("Error -> \(error)")
+         }
+         }
+         
+         task.resume()
+         
+         } catch {
+         print(error)
+         } */
+        
+        
+        /**********BEGIN Sample send request on Your Server -To - MoMo Server
+         **********WARNING: need to remove it on your product app
+         **********/
+        let merchant_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkpa+qMXS6O11x7jBGo9W3yxeHEsAdyDE40UoXhoQf9K6attSIclTZMEGfq6gmJm2BogVJtPkjvri5/j9mBntA8qKMzzanSQaBEbr8FyByHnf226dsLt1RbJSMLjCd3UC1n0Yq8KKvfHhvmvVbGcWfpgfo7iQTVmL0r1eQxzgnSq31EL1yYNMuaZjpHmQuT24Hmxl9W9enRtJyVTUhwKhtjOSOsR03sMnsckpFT9pn1/V9BE2Kf3rFGqc6JukXkqK6ZW9mtmGLSq3K+JRRq2w8PVmcbcvTr/adW4EL2yc1qk9Ec4HtiDhtSYd6/ov8xLVkKAQjLVt7Ex3/agRPfPrNwIDAQAB"
+        let merchant_code       = "SCB01"
+        let merchant_transaction_id = "123456789"
+        let merchant_bill_id        = "bill1001"
+        let merchant_username       = "username_or_email_or_fullname"
+        let merchant_server_ip       = "192.16.17.120"
+        var your_hash_value_sample = "RSA_orderInfo_by_public_key" ;// RSA orderInfo json using MoMo's Public key
+        
+        // tag name to access the stored public key in keychain
+        let TAG_PUBLIC_KEY = ""
+        
+        let hash_plaintext = "{\"merchantcode\":\"\(merchant_code)\",\"transid\":\"\(merchant_transaction_id)\",\"billid\":\"\(merchant_bill_id)\",\"amount\":10000,\"fee\":0,\"phonenumber\":\"\(parram["phonenumber"] as! String)\",\"username\":\"\(merchant_username)\"}"
+        
+        let hash_encrypted = RSAUtils.encryptWithRSAPublicKey(hash_plaintext.data(using: String.Encoding.utf8)!, pubkeyBase64: merchant_public_key, keychainTag: TAG_PUBLIC_KEY)
+        if ( hash_encrypted == nil ) {
+            print("Error while encrypting ")
+        } else {
+            your_hash_value_sample = hash_encrypted!.base64EncodedString(options: NSData.Base64EncodingOptions())
+            print("::Encrypted =  \(your_hash_value_sample)")
+        }
+        
+        
+        let dataPayment : NSMutableDictionary = NSMutableDictionary()
+        dataPayment .setValue(merchant_code,                       forKey: "merchantcode")
+        dataPayment.setValue(your_hash_value_sample,		 forKey: "hash");
+        dataPayment.setValue(merchant_server_ip,		 forKey: "ipaddress");
+        dataPayment.setValue(parram["data"] as! String,		 forKey: "data");
+        dataPayment.setValue(parram["phonenumber"] as! String,	 forKey: "phonenumber");
+        
+        MoMoPayment.sharedInstance.requestPayment(parram: parram)
+        /**********END Sample send request on Your Server -To - MoMo Server
+         **********WARNING: need to remove it on your product app
+         **********/
         
     }
 }
